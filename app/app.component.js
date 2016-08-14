b3c17b7964ce8a432101381c4e94460b3641d97c
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-08-13 19:29:18 -0700

    add app.component.ts

---
+++ app/app.component.js
@@ -0,0 +1,49 @@
+System.register(['angular2/core', './meal-list.component', './meal.model'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, meal_list_component_1, meal_model_1;
+    var AppComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (meal_list_component_1_1) {
+                meal_list_component_1 = meal_list_component_1_1;
+            },
+            function (meal_model_1_1) {
+                meal_model_1 = meal_model_1_1;
+            }],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                    this.meals = [
+                        new meal_model_1.Meal("Bacon Cheeseburger TOASTER", "Get your beef and cheese sandwiched between two thick slices of Texas Toast. Then top it off with crinkle-cut pickles, chopped onions, fresh shredded lettuce, ripe tomatoes, a freshly prepared onion ring & hickory BBQ sauce. It's the ultimate burgwich.", 850, 1490, 14),
+                        new meal_model_1.Meal("Loaded Bacon Ranch Chili Cheese Coney", "The ultimate combination of crispy bacon, chili and melty cheese. The Loaded Bacon Ranch Chili Cheese Coney is made with 100 percent beef and topped with chili, shredded cheese, crisp bacon and zesty ranch.", 510, 1600, 5),
+                        new meal_model_1.Meal("SONIC Blast made with M&M’S Chocolate Candies", "Sweet, cold, real vanilla ice cream blended with colorful M&M’S Chocolate Candies.", 520, 200, 58)
+                    ];
+                }
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'my-app',
+                        directives: [meal_list_component_1.MealListComponent],
+                        template: "\n    <div class=\"main\">\n      <meal-list [mealList]=\"meals\"></meal-list>\n    </div>\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
+                return AppComponent;
+            }());
+            exports_1("AppComponent", AppComponent);
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
---
+++ app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { MealListComponent } from './meal-list.component';
+import { Meal } from './meal.model';
+
+@Component ({
+  selector: 'my-app',
+  directives: [MealListComponent],
+  template: `
+    <div class="main">
+      <meal-list [mealList]="meals"></meal-list>
+    </div>
+  `
+})
+export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Bacon Cheeseburger TOASTER", "Get your beef and cheese sandwiched between two thick slices of Texas Toast. Then top it off with crinkle-cut pickles, chopped onions, fresh shredded lettuce, ripe tomatoes, a freshly prepared onion ring & hickory BBQ sauce. It's the ultimate burgwich.", 850, 1490, 14),
+      new Meal("Loaded Bacon Ranch Chili Cheese Coney", "The ultimate combination of crispy bacon, chili and melty cheese. The Loaded Bacon Ranch Chili Cheese Coney is made with 100 percent beef and topped with chili, shredded cheese, crisp bacon and zesty ranch.", 510, 1600, 5),
+      new Meal("SONIC Blast made with M&M’S Chocolate Candies", "Sweet, cold, real vanilla ice cream blended with colorful M&M’S Chocolate Candies.", 520, 200, 58)
+    ];
+  }
+
+}
